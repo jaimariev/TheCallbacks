cc0dd2105a4bd38999f394ab03abcddf23fb0ec9
Nicholas Reynolds <sonshine_80@yahoo.com>
2015-11-19 13:25:49 -0800

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
-require("../css/normalize.css");
-require("../css/main.css");
+$.ajax({
+  url: 'https://pacific-gorge-8441.herokuapp.com/api/items'
+}).then(function(resp){
+  console.log(resp);
+});
+
+var Container = Backbone.Model.extend({
+  initialize: function(){
+  },
+  defaults: {
+    item_url: null,
+    title: null,
+    description: null,
+    price: null
+  },
+  Model: Container,
+  url: 'https://pacific-gorge-8441.herokuapp.com/api/items'
+});
+
+var Containers = Backbone.Collection.extend({
+   Model: Container,
+   url: 'https://pacific-gorge-8441.herokuapp.com/api/items'
+ });
+
+var CotainerCollection = new Container();
+
+CotainerCollection.fetch ({
+  success: function(resp) {
+    console.log("success", resp);
+  },
+  error: function(err) {
+    console.log("nope", err);
+  }
+});
+
+
+
+
+
+
+
+
 
