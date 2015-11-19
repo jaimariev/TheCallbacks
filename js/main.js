33766a573491194da25eb32e180342738e2932db
Nicholas Reynolds <sonshine_80@yahoo.com>
2015-11-19 15:28:03 -0800

    Uses backbone to get info from the api

--- js/main.js
+++ js/main.js
@@ -1,43 +1,95 @@
-
-$.ajax({
-  url: 'https://pacific-gorge-8441.herokuapp.com/api/items'
-}).then(function(resp){
-  console.log(resp);
-});
-
-var Container = Backbone.Model.extend({
-  initialize: function(){
-  },
-  defaults: {
-    item_url: null,
-    title: null,
-    description: null,
-    price: null
-  },
-  Model: Container,
-  url: 'https://pacific-gorge-8441.herokuapp.com/api/items'
-});
-
-var Containers = Backbone.Collection.extend({
-   Model: Container,
-   url: 'https://pacific-gorge-8441.herokuapp.com/api/items'
- });
-
-var CotainerCollection = new Container();
-
-CotainerCollection.fetch ({
-  success: function(resp) {
-    console.log("success", resp);
-  },
-  error: function(err) {
-    console.log("nope", err);
-  }
-
-
-
-
 $(document).ready(function(){
-	//var Router = require ('./router.jsx');
+
+  $.ajax({
+    url: 'https://pacific-gorge-8441.herokuapp.com/api/items'
+  }).then(function(resp){
+    console.log(resp);
+  });
+
+  var itemContainer = Backbone.Model.extend({
+    initialize: function(){
+    },
+    defaults: {
+      item_url: null,
+      title: null,
+      description: null,
+      price: null
+    },
+    Model: itemContainer,
+    url: 'https://pacific-gorge-8441.herokuapp.com/api/items'
+  });
+
+  var itemContainers = Backbone.Collection.extend({
+     Model: itemContainer
+   });
+
+  var itemCollection = new itemContainer();
+
+  itemCollection.fetch ({
+    success: function(resp) {
+      console.log("success", resp);
+    },
+    error: function(err) {
+      console.log("nope", err);
+    }
+});
+
+  var pledgeContainer = Backbone.Model.extend({
+    initialize: function(){
+    },
+    defaults: {
+      item: null,
+      pledge_amount: null,
+      user: null
+    },
+    Model: pledgeContainer,
+    url: 'https://pacific-gorge-8441.herokuapp.com/api/pledges/'
+  });
+
+  var pledgeContainers = Backbone.Collection.extend({
+    Model: pledgeContainer
+  });
+
+  var pledgeCollection = new pledgeContainer();
+
+  pledgeCollection.fetch ({
+    success: function(resp) {
+      console.log("success", resp);
+    },
+    error: function(err) {
+      console.log("nope", err);
+    }
+  });
+
+  var wishContainer = Backbone.Model.extend({
+    initialize: function(){
+    },
+    defaults: {
+      item: null,
+      expiration_date: null,
+      list_url: null,
+      url: null
+    },
+    Model: wishContainer,
+    url: 'https://pacific-gorge-8441.herokuapp.com/api/wishlists/'
+  });
+
+  var wishContianers = Backbone.Collection.extend({
+    Model: wishContainer
+  });
+
+  var wishCollection = new wishContainer();
+
+  wishCollection.fetch ({
+    success: function(resp) {
+      console.log("success", resp);
+    },
+    error: function(err){
+      console.log("nope", err);
+    }
+  });
+
+
 	console.log('test');
 
 $("#formContainer").hide();
